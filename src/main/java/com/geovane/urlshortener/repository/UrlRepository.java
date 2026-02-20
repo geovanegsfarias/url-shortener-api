bd2152cdac9aa54fd0b9fc8c66678d3c67b82021
Geovane <geovanegsf02@gmail.com>
2026-02-19 22:14:20 -0300

    feat: add UrlRepository

---
+++ src/main/java/com/geovane/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,13 @@
+package com.geovane.urlshortener.repository;
+
+import com.geovane.urlshortener.model.Url;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends CrudRepository<Url, Long> {
+    Optional<Url> findByShortCode(String shortCode);
+    void deleteByShortCode(String shortCode);
+}
